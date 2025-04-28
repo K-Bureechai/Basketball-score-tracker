43c3ebc36984fd97afbb9b3697b711aa75cb9931
May <k.bureechai@gmail.com>
2025-04-28 21:27:32 +0700

    added reset score btns and final styling

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
 //home Btns
-let addOnePointBtn = document.getElementById("plus-one")
-let defultScoreHome = document.getElementById("home-score")
+let addOnePointBtn = document.getElementById("plus-one");
+let defultScoreHome = document.getElementById("home-score");
 
-let beforeScoreHome = 0
+let beforeScoreHome = 0;
 
 function addOnePointHome() {
-    beforeScoreHome += 1
-  defultScoreHome.textContent = beforeScoreHome
+  beforeScoreHome += 1;
+  defultScoreHome.textContent = beforeScoreHome;
 }
 
 function addTwoPointHome() {
-    beforeScoreHome += 2
-  defultScoreHome.textContent = beforeScoreHome
+  beforeScoreHome += 2;
+  defultScoreHome.textContent = beforeScoreHome;
 }
 
 function addThreePointHome() {
-    beforeScoreHome += 3
-  defultScoreHome.textContent = beforeScoreHome
+  beforeScoreHome += 3;
+  defultScoreHome.textContent = beforeScoreHome;
+}
+// reset Btn Home
+let resetScoresHome = document.getElementById("reset-home");
+function resetScoreHome() {
+  defultScoreHome.textContent = 0;
+  beforeScoreHome = 0;
 }
 
 //guest Btns
-let defultScoreGuest = document.getElementById("guest-score")
-let beforeScoreGuest = 0
+let defultScoreGuest = document.getElementById("guest-score");
+let beforeScoreGuest = 0;
 
 function addOnePointGuest() {
-    beforeScoreGuest += 1
-  defultScoreGuest.textContent = beforeScoreGuest
+  beforeScoreGuest += 1;
+  defultScoreGuest.textContent = beforeScoreGuest;
 }
 function addTwoPointGuest() {
-    beforeScoreGuest += 2
-  defultScoreGuest.textContent = beforeScoreGuest
+  beforeScoreGuest += 2;
+  defultScoreGuest.textContent = beforeScoreGuest;
 }
 function addThreePointGuest() {
-    beforeScoreGuest += 3
-  defultScoreGuest.textContent = beforeScoreGuest
+  beforeScoreGuest += 3;
+  defultScoreGuest.textContent = beforeScoreGuest;
+}
+// reset Btn Guest
+let resetScoresGuest = document.getElementById("reset-guest");
+function resetScoreGuest() {
+  defultScoreGuest.textContent = 0;
+  beforeScoreGuest = 0;
 }
